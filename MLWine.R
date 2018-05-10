add6c469fc25cbeb710003a24b38a3ded9053759
jcm300 <zeclmartins@gmail.com>
2018-05-10 20:51:13 +0100

    attempted to minimize error

--- MLWine.R
+++ MLWine.R
@@ -25,24 +25,24 @@ dadosTeste <- dadosR[4501:6497, ]
 
 # definição das camadas de entrada e saída da RNA
 funcao <- quality ~ fixed.acidity+volatile.acidity+citric.acid+residual.sugar+chlorides+free.sulfur.dioxide+total.sulfur.dioxide+density+pH+sulphates+alcohol
-funcaoOpt <- quality ~ alcohol+volatile.acidity+sulphates+residual.sugar+total.sulfur.dioxide
-funcaoOpt2 <- quality ~ alcohol+volatile.acidity+sulphates
 
 selecao1<-regsubsets(funcao,dadosR,nvmax=11)
 summary(selecao1)
 selecao2<-regsubsets(funcao,dadosR,nvmax=5)
 summary(selecao2)
 
+funcaoOpt <- quality ~ alcohol+volatile.acidity+sulphates+residual.sugar+total.sulfur.dioxide
+funcaoOpt2 <- quality ~ alcohol+volatile.acidity+sulphates
+
 # treinar a rede neuronal 
-# only using 'funcao' at this moment
-rnaWine1 <- neuralnet( funcao, dadosTreino, lifesign="full", hidden = c(7,4), threshold = 0.1)
-rnaWine2 <- neuralnet( funcao, dadosTreino, lifesign="full", hidden = c(10,8), threshold = 0.1)
-rnaWine3 <- neuralnet( funcao, dadosTreino, lifesign="full", hidden = c(3,3), threshold = 0.1)
+rnaWine1 <- neuralnet( funcao, dadosTreino, lifesign="full", hidden = c(7,5), threshold = 0.01)
+rnaWine2 <- neuralnet( funcaoOpt, dadosTreino, lifesign="full", hidden = c(4,2), threshold = 0.01)
+rnaWine3 <- neuralnet( funcaoOpt2, dadosTreino, lifesign="full", hidden = c(5), threshold = 0.01)
 
 # definir variaveis de input para teste
 dadosTeste1 <- subset(dadosTeste, select = c("fixed.acidity","volatile.acidity","citric.acid","residual.sugar","chlorides","free.sulfur.dioxide","total.sulfur.dioxide","density","pH","sulphates","alcohol"))
-dadosTeste2 <- subset(dadosTeste, select = c("fixed.acidity","volatile.acidity","citric.acid","residual.sugar","chlorides","free.sulfur.dioxide","total.sulfur.dioxide","density","pH","sulphates","alcohol"))
-dadosTeste3 <- subset(dadosTeste, select = c("fixed.acidity","volatile.acidity","citric.acid","residual.sugar","chlorides","free.sulfur.dioxide","total.sulfur.dioxide","density","pH","sulphates","alcohol"))
+dadosTeste2 <- subset(dadosTeste, select = c("alcohol","volatile.acidity","sulphates","residual.sugar","total.sulfur.dioxide"))
+dadosTeste3 <- subset(dadosTeste, select = c("alcohol","volatile.acidity","sulphates")) 
 
 # testar a rede com os novos casos
 rnaWine1.resultados <- compute(rnaWine1,dadosTeste1)
